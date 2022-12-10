510c67ef96684d924dc93995a11291dc64e284f7
Jeong WooSeok <wjddntjr555w@naver.com>
2022-12-10 09:00:05 +0900

    added Shipping charges.

--- src/main/java/org/example/service/OrderService.java
+++ src/main/java/org/example/service/OrderService.java
@@ -9,9 +9,9 @@ public class OrderService {
     public static boolean order() {
 
         Data data = DataRepository.showData();
-//        System.out.println(data.toString());
         Item item = CartRepository.showItem();
 
+        int sumOrder = 0;
         for(int i = 0; i< item.getCom().size(); i++) {
             for(int j = 0; j < data.getDataNum().size(); j++){
                 if(item.getCom().get(i).equals(data.getDataNum().get(j))){
@@ -22,11 +22,17 @@ public class OrderService {
                     else{
                         int temp = Integer.valueOf(String.valueOf(data.getDataCount().get(j))) - Integer.valueOf(String.valueOf(item.getNum().get(i)));
                         data.setDataCountIndex(data.getDataCount(),j,temp);
+                        sumOrder += (Integer) item.getNum().get(i) * (Integer) data.getDataPrice().get(j);
                         System.out.println(data.getDataName().get(j) + " - "+item.getNum().get(i)+"개");
                     }
                 }
             }
         }
+        if(sumOrder<50000)
+            sumOrder += 2500;
+        System.out.println("총 결제 금액(배송비 포함) : "+ sumOrder);
         return true;
     }
+
+
 }
